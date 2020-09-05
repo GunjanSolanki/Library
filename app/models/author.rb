393ed865f8dacb3a56aaf817c1bbe9d84d446571
Gunjan Solanki <solanki.gunjan007@gmail.com>
2020-09-06 00:09:24 +0530

    Add method to get full name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
   has_many :author_books, dependent: :destroy
   has_many :books, through: :author_books 
+
+  def full_name
+    [first_name, last_name].compact.join(' ')
+  end
 end
